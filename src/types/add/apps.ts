77d641cee21b0cdae954d796dd032636d0b5f6fc
Isaac Guerreiro <isaac@igem.org>
2022-10-23 14:29:56 -0300

    type app and create app schema

---
+++ src/types/add/apps.ts
@@ -0,0 +1,18 @@
+import { object, string, nativeEnum, TypeOf } from "zod";
+import { Visibility } from '@prisma/client'
+import { errorFieldValidation, errorStringValidation } from "../../utils/error";
+
+export const createAppSchema = object({
+    name: string(errorFieldValidation("Name field is required.", "Invalid Type: Name could only be a string type."))
+      .min(3, errorStringValidation("Name field must have at least 3 characters."))
+      .max(25, errorStringValidation("Name field must have less than 25 characters.")),
+    description: string()
+      .min(3, errorStringValidation("Name field must have at least 3 characters."))
+      .max(255, errorStringValidation("Name field must have less than 255 characters.")),
+    url: string()
+      .url(errorStringValidation("This field is an invalid URL."))
+      .nullish(),
+    visibility: nativeEnum(Visibility) 
+  }).required()
+
+export type CreateAppInput = TypeOf<typeof createAppSchema>

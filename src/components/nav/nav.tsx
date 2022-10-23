9162699ce79049130c15a6bf696847cbcab1dc4b
Isaac Guerreiro <isaac@igem.org>
2022-10-23 14:26:40 -0300

    create components

---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,96 @@
+import { Session } from "next-auth"
+import { Popover } from '@headlessui/react'
+import { signIn, signOut, useSession } from "next-auth/react"
+import defaultPhoto from 'https://astrolabium.s3.sa-east-1.amazonaws.com/assets/default_photo.jpg'
+import Image, { StaticImageData } from "next/image"
+
+export const Nav = () => {
+  const { data: session } = useSession()
+
+  return (
+  	<nav className="bg-gray-800">
+			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+				<div className="relative flex h-16 items-center justify-between">
+					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						<button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+							<span className="sr-only">Open main menu</span>
+							<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+							</svg>
+							<svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</button>
+					</div>
+					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="flex flex-shrink-0 items-center">
+              {
+                /*
+                  <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                  <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                */
+              }
+						</div>
+						<div className="hidden sm:ml-6 sm:block">
+							<div className="flex space-x-4">
+							</div>
+						</div>
+					</div>
+					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            { session ? <Profile session={session} /> : <SignInButton />  }  
+					</div>
+				</div>
+			</div>
+
+			<div className="sm:hidden" id="mobile-menu">
+				<div className="space-y-1 px-2 pt-2 pb-3">
+				</div>
+			</div>
+		</nav> 
+  )
+}
+const SignInButton = () => {
+  return (
+    <div className="relative ml-3">
+      <div>
+        <button type="button" onClick={() => signIn()} className="flex rounded-full bg-gray-800 text-sm font-medium text-base text-gray-300" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+          Sign In
+          <div className="sr-only">Sign In</div>
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+const Profile = ({ session }: { session: Session }) => {
+  const { user } = session
+  
+  return (
+    <Popover>
+    <div className="relative ml-3">
+      <div>
+        <Popover.Button type="button" className="h-8 w-8 flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+          <span className="sr-only">Open user menu</span>
+          <ProfilePicture staticImage={user?.image} />
+        </Popover.Button>
+      </div>
+
+      <Popover.Panel className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
+        <div className="block px-4 py-2 text-sm text-gray-700 border-b" >Hi, { session.user?.name }</div>
+        <div className="flex flex-col justify-end items-end">
+          <button className="block px-4 py-2 text-sm text-gray-700">Settings</button>
+          <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signOut()}>Sign out</button>
+        </div>
+      </Popover.Panel>
+    </div>
+    </Popover>
+  )
+}
+
+const ProfilePicture = ({ staticImage }: { staticImage: StaticImageData | string | undefined | null }) => {
+  if (staticImage) {
+    return <Image className="rounded-full" layout='fill' src={staticImage} alt="" />
+  }
+  return <Image className="rounded-full" layout='fill' src={defaultPhoto} alt="" />
+ }

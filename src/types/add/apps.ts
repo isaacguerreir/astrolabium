ea5197c6ca6415102cf1c7e68add27aeda2cfc32
isaacguerreir <isaacguerreirocom@gmail.com>
2022-10-24 01:17:03 -0300

    Application zod validator checks new appName and component field

--- src/types/add/apps.ts
+++ src/types/add/apps.ts
@@ -10,8 +10,11 @@ export const createAppSchema = object({
       .min(3, errorStringValidation("Name field must have at least 3 characters."))
       .max(255, errorStringValidation("Name field must have less than 255 characters.")),
     url: string()
-      .url(errorStringValidation("This field is an invalid URL."))
-      .nullish(),
+      .url(errorStringValidation("This field is an invalid URL.")),
+    appName: string(errorFieldValidation("Application Name field is required.", "Invalid Type: Application Name could only be a string type."))
+      .min(3, errorStringValidation("Name field must have at least 3 characters.")),
+    component: string(errorFieldValidation("Component field is required.", "Invalid Type: Component could only be a string type."))
+      .min(3, errorStringValidation("Name field must have at least 3 characters.")),
     visibility: nativeEnum(Visibility) 
   }).required()
 
